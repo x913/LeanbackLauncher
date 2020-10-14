b16e0b7eb4b932b664449040425559d182a36584
x913 <x913x913@gmail.com>
2020-10-14 13:07:56 +0500

    Fix issue with memory cleaning

--- app/src/main/java/com/amazon/tv/leanbacklauncher/CleanDeviceMemory.java
+++ app/src/main/java/com/amazon/tv/leanbacklauncher/CleanDeviceMemory.java
@@ -27,17 +27,18 @@ public class CleanDeviceMemory extends Activity {
         mThread = new Thread(new Runnable() {
             @Override
             public void run() {
-                // killing background processes
+                ActivityManager activityManager = (ActivityManager)getApplicationContext().getSystemService(ACTIVITY_SERVICE);
                 PackageManager pm = getPackageManager();
-                List<ApplicationInfo> installedPackages = pm.getInstalledApplications(0);
-                ActivityManager mActivityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-                for (ApplicationInfo applicationInfo : installedPackages) {
-                    if ((applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 1)
-                        continue;
-                    if(getApplicationContext().getPackageName().equalsIgnoreCase(applicationInfo.packageName))
-                        continue;
-                    mActivityManager.killBackgroundProcesses(applicationInfo.packageName);
+                List<ApplicationInfo> pi = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+                for(ApplicationInfo ai: pi) {
+                    // todo add if(getApplicationContext().getPackageName().equalsIgnoreCase(applicationInfo.packageName))
+                    if(ai.packageName.equalsIgnoreCase("com.amazon.tv.leanbacklauncher")) {
+
+                    } else {
+                        activityManager.killBackgroundProcesses(ai.packageName);
+                    }
                 }
+
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
